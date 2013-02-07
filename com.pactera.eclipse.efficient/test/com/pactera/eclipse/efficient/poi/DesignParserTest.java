dd8771364b7420215d0030f114363e359ff3c0a5
junwing <r.junwing@gmail.com>
2013-02-07 11:27:53 +0800

    sequence test

--- com.pactera.eclipse.efficient/test/com/pactera/eclipse/efficient/poi/DesignParserTest.java
+++ com.pactera.eclipse.efficient/test/com/pactera/eclipse/efficient/poi/DesignParserTest.java
@@ -19,4 +19,13 @@ public class DesignParserTest {
 		}
 	}
 
+	@Test
+	public void testParseDBFileForSequence() throws IOException {
+		String path = "E:\\workspaces\\grcb\\项目文档\\21_开发记录\\DY20120809-001_企业基金代销\\03_网银项目组_企业基金_tables.xls";
+		List<Table> tables = DesignParser.parseDBFile(new File(path));
+		for (Table table : tables) {
+			System.out.println(table.toSequence());
+		}
+	}
+
 }
